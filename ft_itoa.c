d023907f271cac76439c92e0798690e44357833f
anas712066 <anasabid607@gmail.com>
2024-10-16 00:27:43 +0200

    push pipeline

--- ft_itoa.c
+++ ft_itoa.c
@@ -3,58 +3,81 @@
 /*                                                        :::      ::::::::   */
 /*   ft_itoa.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mumajeed <mumajeed@student.42barcelon      +#+  +:+       +#+        */
+/*   By: alromero <alromero@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/10/15 09:12:43 by mumajeed          #+#    #+#             */
-/*   Updated: 2024/10/15 12:00:11 by mumajeed         ###   ########.fr       */
+/*   Created: 2019/11/12 20:02:31 by alromero          #+#    #+#             */
+/*   Updated: 2024/10/16 00:26:53 by mumajeed         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static int	ft_numlen(int n)
+static int	ft_digit_counter(int n)
 {
-	int	len;
+	int	i;
+	int	copy;
 
-	len = 0;
+	i = 0;
+	if (n == 0)
+		return (1);
+	if (n == -2147483648)
+		return (11);
 	if (n < 0)
-		len++;
-	while (n / 10 != 0)
 	{
-		len++;
-		n /= 10;
+		n = -n;
+		i++;
 	}
-	return (len);
+	copy = n;
+	while (copy > 0)
+	{
+		copy = copy / 10;
+		i++;
+	}
+	return (i);
+}
+
+static int	ft_exp_counter(int n)
+{
+	int	exp;
+	int	digits;
+
+	if (n == 0)
+		return (1);
+	if (n == -2147483648)
+		return (1000000000);
+	digits = ft_digit_counter(n);
+	if (n < 0)
+		digits--;
+	exp = 1;
+	while (--digits)
+		exp = exp * 10;
+	return (exp);
 }
 
 char	*ft_itoa(int n)
 {
-	char	*str;
-	int		len;
-	long	num;
+	char		*str;
+	int			exp;
+	int			i;
+	long int	copy;
 
-	num = n;
-	len = ft_numlen(num);
-	str = (char *)malloc(sizeof(char) * (len + 1));
+	copy = (long int)n;
+	exp = ft_exp_counter(n);
+	i = 0;
+	str = malloc(ft_digit_counter(n) + 1);
 	if (!str)
 		return (NULL);
-	str[len] = '\0';
-	if (num < 0)
+	if (n < 0)
 	{
-		str[0] = '-';
-		num = -num;
+		str[i++] = '-';
+		copy = -copy;
 	}
-	while (len-- > 0) //&& str[len] != '-')
+	while (exp > 0)
 	{
-		str[len] = num % 10 + '0';
-		num /= 10;
+		str[i++] = (copy / exp) + 48;
+		copy = copy % exp;
+		exp = exp / 10;
 	}
+	str[i] = '\0';
 	return (str);
-}
-int	main(void)
-{
-	int	numeros = -123;
-	
-	printf("%s\n", ft_itoa(numeros));
-	return (0);
 }
