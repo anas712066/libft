29a7301221b5a6838d03eccda038d90d11ca4fbb
Muhammad Anas Majeed Rafique <mumajeed@car1s4.42barcelona.com>
2024-10-15 09:48:48 +0200

    push pipeline

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: mumajeed <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/25 17:53:12 by mumajeed          #+#    #+#             */
-/*   Updated: 2024/10/14 00:03:13 by mumajeed         ###   ########.fr       */
+/*   Updated: 2024/10/15 09:48:38 by mumajeed         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -49,6 +49,6 @@ char	*ft_substr(char const *s, unsigned int start, size_t len);
 char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_strtrim(char const *s1, char const *set);
 char	**ft_split(char const *s, char c);
-int     ft_atoi(const char *nptr);
+int		ft_atoi(const char *nptr);
 
 #endif
